2b95abeadf66379c9317937f719d85549014d986
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-10-30 06:53:03 +0500

    Initial commit

---
+++ app/src/main/java/com/mesnuuu/tiptime/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.mesnuuu.tiptime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mesnuuu.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        // If the cost is null or 0, then display 0 tip and exit this function early.
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_20_pecent -> 0.20
+            R.id.option_18_pecent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        // Display the formatted tip value on screen
+        displayTip(tip)
+    }
+
+    fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
